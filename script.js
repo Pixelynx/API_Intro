cf27d9570351d42e6c569b8527cc03768f9696bf
Pixelynx <tjstevens1992@gmail.com>
2018-11-15 19:13:31 -0500

    almost done

--- script.js
+++ script.js
@@ -123,4 +123,95 @@ let APIResponse = {
   }
 };
 
-let breedList = [];
+
+
+
+
+const breedList = [];
+
+const list = () => {
+
+  let dogsObj = APIResponse['message'];
+
+  for(let key in dogsObj){
+    if(dogsObj[key].length){
+      dogsObj[key].forEach((dog) => {
+        key = key.split(' ');
+        key = key[0][0].toUpperCase().concat(key[0].slice(1));
+          dog = dog.split(' ');
+          dog = dog[0][0].toUpperCase().concat(dog[0].slice(1));
+            breedList.push(`${dog} ${key}`)
+      })
+    }
+    else {
+      key = key.split(' ');
+      key = key[0][0].toUpperCase().concat(key[0].slice(1));
+        breedList.push(key)
+    }
+  }
+  return breedList;
+}
+
+const randomizeDogs = () => {
+  let theList = list();
+  let index = Math.floor(Math.random() * theList.length);
+  return breedList[index];
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  let button = document.querySelector('#button');
+  let body = document.querySelector('body');
+  let printBreed = document.createElement('p');
+
+  button.addEventListener('click', () => {
+
+    printBreed.innerHTML = randomizeDogs();
+    body.appendChild(printBreed);
+
+
+  })
+  button.addEventListener('click', () => {
+    // let replaceBreed = document.createElement('p');
+
+    printBreed.textContent = randomizeDogs();
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const breeds = (breedList) => {
+//   breedList = [];
+//   let data = APIResponse['message'];
+//
+//   Object.keys(data).forEach((dog) => {
+//     if(data[dog].length) {
+//       data[dog].forEach((type) => {
+//         breedList.push(`${type} ${dog}`);
+//       })
+//     } else {
+//       breedList.push(`${dog}`);
+//     }
+//   })
+//   return breedList;
+// }
+
+// console.log(breeds());
